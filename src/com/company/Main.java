3163b4da7d879404c1b3b51236e32181fe4880b7
Johannes <johannes@johannesskagius@gmail.com>
2022-02-10 18:11:25 +0100

    everything works, cleaned

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,10 @@
 package com.company;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
     private final Downloads downloads = new Downloads ();
-    private Map<Integer, String> stopIDToName = new HashMap<> ();
 
     public static void main (String[] args) {
         Main main = new Main ();
@@ -23,7 +21,7 @@ public class Main {
     void run () {
         //Download stops
         try {
-            stopIDToName = downloads.getStops ();
+            Map<Integer, String> stopIDToName = downloads.getStops ();
             BussLines bussLines = new BussLines ( stopIDToName, downloads );
             bussLines.getBussLines ();
             bussLines.printTopScorers();
